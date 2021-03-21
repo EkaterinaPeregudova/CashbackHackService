52ad4cf8e0891a52a726335429b8055571c5fd59
katerina-kis <katerina-kis@yandex.ru>
2021-03-22 00:24:16 +0300

    add Test, TestNG

---
+++ src/test/java/ru/netology/service/CashbackHackServiceTest.java
@@ -0,0 +1,42 @@
+package ru.netology.service;
+
+import org.testng.annotations.Test;
+
+import static org.testng.Assert.*;
+
+class CashbackHackServiceTest {
+    CashbackHackService service = new CashbackHackService();
+    @Test
+   public void showBelowBoundary(){
+        int purchaseAmount = 600;
+        int actualResult = service.remain(purchaseAmount);
+        int expectedResult = 400;
+        assertEquals(actualResult, expectedResult);
+
+    }
+    @Test
+    public void showHigherBoundary(){
+        int purchaseAmount = 1300;
+        int actualResult = service.remain(purchaseAmount);
+        int expectedResult = 700;
+        assertEquals(actualResult, expectedResult);
+
+    }@Test
+    public void showNallBoundary(){
+        int purchaseAmount = 0;
+        int actualResult = service.remain(purchaseAmount);
+        int expectedResult = 1000;
+        assertEquals(actualResult, expectedResult);
+
+    }
+
+}@Test
+public void showEqualBoundary(){
+    int purchaseAmount = 1000;
+    int actualResult = service.remain(purchaseAmount);
+    int expectedResult = 1000;
+    assertEquals(actualResult, expectedResult);
+
+}
+
+}
